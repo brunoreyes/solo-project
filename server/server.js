893fe7cc17b43abc7f808968e7d69a21f05ea580
“brunoreyes” <bruno619reyes@gmail.com>
2020-08-01 16:09:26 -0500

    initial commit, successfully sent img to database

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const bodyParser = require('body-parser');
+const sessionMiddleware = require('./modules/session-middleware');
+
+const passport = require('./strategies/user.strategy');
+
+// Route includes
+const userRouter = require('./routes/user.router');
+// UploaderS3Router added below
+const UploaderS3Router = require('react-dropzone-s3-uploader/s3router');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Routes */
+app.use('/api/user', userRouter);
+// AWS S3 image upload
+app.use(
+  '/s3',
+  UploaderS3Router({
+    bucket: 'brunobucket', // required
+    region: 'us-east-1', // optional
+    headers: { 'Access-Control-Allow-Origin': '*' }, // optional
+    ACL: 'public-read', // this is the default - set to `public-read` to let anyone view uploads
+  })
+);
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
